d9592c845baa992b0f4d1059373b2758a03e3e9a
nhatminhit <31946502+nhatminhit@users.noreply.github.com>
2021-12-12 20:47:23 +0700

    tuan12

---
+++ Tuan12/thuchanh/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using thuchanh.Models;
+namespace thuchanh
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        QLBanHanggContext db = new QLBanHanggContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Loaded(object sender, RoutedEventArgs e)
+        {
+            hienthi();
+            var queryCombo = from lsp in db.LoaiSps
+                             select lsp;
+            cboLoai.ItemsSource = queryCombo.ToList();
+        }
+        private void hienthi()
+        {
+            var query = from sp in db.SanPhams
+                             select sp;
+            dgvSanPham.ItemsSource = query.ToList();
+        }
+        private void setOnClick_btnThem(object sender, RoutedEventArgs e)
+        {
+            SanPham spMoi = new SanPham();
+            spMoi.MaSp = txtMa.Text;
+            spMoi.TenSp = txtTen.Text;
+            spMoi.SoLuongCo = int.Parse(txtSoLuongCo.Text);
+            spMoi.DonGia = int.Parse(txtDonGia.Text);
+            LoaiSp lspChon = (LoaiSp)cboLoai.SelectedItem;
+            spMoi.MaLoai = lspChon.MaLoai;
+            db.SanPhams.Add(spMoi);
+            db.SaveChanges();
+            hienthi();
+        }
+        private void setOnClick_btnSua(object sender, RoutedEventArgs e)
+        {
+            var query = from sp in db.SanPhams
+                        where sp.MaSp == txtMa.Text
+                        select sp;
+            SanPham spSua = query.FirstOrDefault();
+            spSua.TenSp = txtTen.Text;
+            db.SaveChanges();
+            hienthi();
+        }
+        private void setOnClick_btnXoa(object sender, RoutedEventArgs e)
+        {
+            var query = from sp in db.SanPhams
+                        where sp.MaSp == txtMa.Text
+                        select sp;
+            SanPham spXoa = query.FirstOrDefault();
+            db.SanPhams.Remove(spXoa);
+            db.SaveChanges();
+            hienthi();
+        }
+        private void setOnClick_btnTim(object sender, RoutedEventArgs e)
+        {
+            var query = from sp in db.SanPhams
+                        where sp.DonGia > 1000
+                        select sp;
+            dgvSanPham.ItemsSource = query.ToList();
+        }
+    }
+}
---
+++ Tuan12/thuchanh/Models/QLBanHanggContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace thuchanh.Models
+{
+    public partial class QLBanHanggContext : DbContext
+    {
+        public QLBanHanggContext()
+        {
+        }
+
+        public QLBanHanggContext(DbContextOptions<QLBanHanggContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<HoaDonChiTiet> HoaDonChiTiets { get; set; }
+        public virtual DbSet<LoaiSp> LoaiSps { get; set; }
+        public virtual DbSet<SanPham> SanPhams { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-IH8FCR0\\SQLEXPRESS;Initial Catalog=QLBanHangg;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<HoaDonChiTiet>(entity =>
+            {
+                entity.HasKey(e => new { e.MaHd, e.MaSp })
+                    .HasName("PK__HoaDonCh__F557F661670DBF9B");
+
+                entity.ToTable("HoaDonChiTiet");
+
+                entity.Property(e => e.MaHd)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("MaHD")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.MaSp)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("MaSP")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.NgayBan).HasColumnType("date");
+
+                entity.HasOne(d => d.MaSpNavigation)
+                    .WithMany(p => p.HoaDonChiTiets)
+                    .HasForeignKey(d => d.MaSp)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__HoaDonChiT__MaSP__15502E78");
+            });
+
+            modelBuilder.Entity<LoaiSp>(entity =>
+            {
+                entity.HasKey(e => e.MaLoai)
+                    .HasName("PK__LoaiSP__730A57599F0B48F2");
+
+                entity.ToTable("LoaiSP");
+
+                entity.Property(e => e.MaLoai)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.TenLoai).HasMaxLength(20);
+            });
+
+            modelBuilder.Entity<SanPham>(entity =>
+            {
+                entity.HasKey(e => e.MaSp)
+                    .HasName("PK__SanPham__2725081C79AB471B");
+
+                entity.ToTable("SanPham");
+
+                entity.Property(e => e.MaSp)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .HasColumnName("MaSP")
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.MaLoai)
+                    .HasMaxLength(3)
+                    .IsUnicode(false)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.TenSp)
+                    .HasMaxLength(30)
+                    .HasColumnName("TenSP");
+
+                entity.HasOne(d => d.MaLoaiNavigation)
+                    .WithMany(p => p.SanPhams)
+                    .HasForeignKey(d => d.MaLoai)
+                    .HasConstraintName("FK__SanPham__MaLoai__1273C1CD");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
